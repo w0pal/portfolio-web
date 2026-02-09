62dd17030abcfdf9a2cd2a76bf5fc593bff05281
w0pal <mnaufalmaulana84@gmail.com>
2026-02-09 21:43:23 +0700

    fix: allow email account link for oauth

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -13,11 +13,13 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     // Google - for regular users (commenters)
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   callbacks: {
